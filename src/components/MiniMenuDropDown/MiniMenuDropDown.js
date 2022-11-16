52a05f7cd041cd203be55894a63cf96e343e6e5c
tljun <tljun@kth.se>
2022-11-16 13:18:47 +0100

    Fixed too many categories overflow

--- src/components/MiniMenuDropDown/MiniMenuDropDown.js
+++ src/components/MiniMenuDropDown/MiniMenuDropDown.js
@@ -111,7 +111,7 @@ export const MiniMenuDropDown = forwardRef((props,ref) => {
         return (
         <div className="mini-menu-dropdown">
             <div>{groceryList[miniMenuDroppedDown.index].item}</div>
-            {categories().map(category => <div className="mini-menu-dropdown-item" onClick={() => handleItemCategoryChange(category)}>{category}</div>)}
+            {categories().length > 0 ? <div className="mini-menu-categories-list">{categories().map(category => <div className="mini-menu-dropdown-item" onClick={() => handleItemCategoryChange(category)}>{category}</div>)}</div> : null}
             <div className="mini-menu-dropdown-item" onClick={() => handleItemCategoryChange(null)}>..New category</div>
             <div className="mini-menu-dropdown-item" onClick={handleCategories}><BiRightArrow className="mini-menu-dropdown-arrow"/>Back</div>
             <div className="mini-menu-dropdown-item" onClick={() => setMiniMenuDroppedDown({droppedDown: false, index: null, list: "groceryList"})}>Close</div>
